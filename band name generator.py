65300253f88af9080e1fbb615cc0383b83c6e5c4
Debora Mendes <debora.tmsilvaa@gmail.com>
2025-07-04 12:33:56 -0300

    Add files via upload

---
+++ band name generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city =input("Which city did you grow up in?\n")
+pet = input("Whats is the name of a pet? \n")
+print("your band name cloud be: "+city +" "+pet)
---
+++ udemy1.py
@@ -0,0 +1,5 @@
+print("hello"+"Angela") 
+print("Hello "+ input("What is your name? ") +"!!")
+print('My name is')
+
+print(len(input("Qual o seu nome")))
